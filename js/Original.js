44363b68c4add8a11fd42047267689eabe261644
satoshi <satoshidat@gmail.com>
2018-07-11 19:30:22 +0900

    画面比のインナーイベントハンドラを消去しました

--- js/Original.js
+++ js/Original.js
@@ -1,41 +1,44 @@
 /*
 ウィンドウサイズを変更した際に画面幅を変更する関数
 */
+
 function responsiv()
  {
    var widthsize=document.body.clientWidth;//現在のウィンドウサイズの取得
    var main= document.getElementById('main');//idの取得
    var side= document.getElementById("side");
    var  maincont= document.getElementById('maincont');//グローバル変数
-   main.clientWidth=widthsize;//現在のウィンドウサイズを最大ウィンドウ幅に代入
-    var haba=widthsize*0.2;
-    side.style.width=haba+"px";
-    maincont.style.left=side.style.width;
-    maincont.style.width=widthsize-haba+"px";
+   main.style.minWidth=480+"px";
+   if(parseInt(widthsize)>=480)
+   {
+     main.clientWidth=widthsize;//現在のウィンドウサイズを最大ウィンドウ幅に代入
+     var haba=widthsize*0.2;
+     side.style.width=haba+"px";
+     maincont.style.left=side.style.width;
+     maincont.style.width=widthsize-haba+"px";
+   }
+   else if(parseInt(widthsize)<480)
+   {
+     side.style.width=100+"px";
+      maincont.style.left=side.style.width;
+   }
+
  }
 
  //TOPの画像表示範囲の設定。
 
 
 
+
 //マウスを枠の中に入れたらTOP画像が変わる仕組み。
- function FUNCIMG (img,url)
+ function FUNCIMG (img)
 {
-
-  var ancurl=document.getElementById('AncerUrl');
+  var ancurl=document.querySelector('.ImgItem');
+  //ancurl.onmouseover=this.src;
   var imgsrc=document.getElementById('TopFrame');
-
-
-if(imgsrc.src!=img)
-  {
-    imgsrc.style.opacity=0;
-    //無名関数にアロー関数（ラムダ式）を使うことで関数内ををreturnで返すことができる。
-    setTimeout(()=>{imgsrc.style.opacity=1;imgsrc.src=img;ancurl.href=url;},500);
-  }
-  else
-  {
-      return;
-  }
+  imgsrc.style.opacity=0;
+  //無名関数にアロー関数（ラムダ式）を使うことで関数内ををreturnすることができる。
+  setTimeout(()=>{imgsrc.style.opacity=1;imgsrc.src=ancurl.src;},500);
 }
 
 /*
@@ -43,7 +46,7 @@ if(imgsrc.src!=img)
 */
 
 var cnt=0;
-
+//addEventListener('click',PlusScr());
 function PlusScr()
 {
  var min=0;
@@ -77,3 +80,6 @@ if(cnt==0)
 
 
 }
+/*関数呼び出し*/
+window.onload=responsiv;
+window.onresize=responsiv;
